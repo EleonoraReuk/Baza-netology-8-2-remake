1386b769fbe20a1dda01937d4a7839e11c152857
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-06-04 14:15:43 +0700

    Update Parallelogram.cpp

--- Parallelogram.cpp
+++ Parallelogram.cpp
@@ -8,18 +8,18 @@ Parallelogram::Parallelogram(int a, int b, int A, int B)
 void Parallelogram::validate() const {
     Quadrilateral::validate();
     if (a != c || b != d) {
-        throw FigureException("Параллелограмм", "Противоположные стороны должны быть равны");
+        throw FigureException("РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј", "РџСЂРѕС‚РёРІРѕРїРѕР»РѕР¶РЅС‹Рµ СЃС‚РѕСЂРѕРЅС‹ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ СЂР°РІРЅС‹");
     }
     if (A != C || B != D) {
-        throw FigureException("Параллелограмм", "Противоположные углы должны быть равны");
+        throw FigureException("РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј", "РџСЂРѕС‚РёРІРѕРїРѕР»РѕР¶РЅС‹Рµ СѓРіР»С‹ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ СЂР°РІРЅС‹");
     }
     if (A + B != 180) {
-        throw FigureException("Параллелограмм", "Сумма соседних углов должна быть 180 градусов");
+        throw FigureException("РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј", "РЎСѓРјРјР° СЃРѕСЃРµРґРЅРёС… СѓРіР»РѕРІ РґРѕР»Р¶РЅР° Р±С‹С‚СЊ 180 РіСЂР°РґСѓСЃРѕРІ");
     }
 }
 
 void Parallelogram::print_info() const {
-    std::cout << "Параллелограмм:" << std::endl;
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј:" << std::endl;
+    std::cout << "РЎС‚РѕСЂРѕРЅС‹: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
+    std::cout << "РЈРіР»С‹: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
 }
