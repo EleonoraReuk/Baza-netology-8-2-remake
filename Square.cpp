19ce78fbf7164165ca22da7dfd48b5800b83498e
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-06-04 14:17:35 +0700

    Update Square.cpp

--- Square.cpp
+++ Square.cpp
@@ -8,15 +8,15 @@ Square::Square(int side)
 void Square::validate() const {
     Quadrilateral::validate();
     if (a != b || b != c || c != d) {
-        throw FigureException("Квадрат", "Все стороны должны быть равны");
+        throw FigureException("РљРІР°РґСЂР°С‚", "Р’СЃРµ СЃС‚РѕСЂРѕРЅС‹ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ СЂР°РІРЅС‹");
     }
     if (A != 90 || B != 90 || C != 90 || D != 90) {
-        throw FigureException("Квадрат", "Все углы должны быть 90 градусов");
+        throw FigureException("РљРІР°РґСЂР°С‚", "Р’СЃРµ СѓРіР»С‹ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ 90 РіСЂР°РґСѓСЃРѕРІ");
     }
 }
 
 void Square::print_info() const {
-    std::cout << "Квадрат:" << std::endl;
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "РљРІР°РґСЂР°С‚:" << std::endl;
+    std::cout << "РЎС‚РѕСЂРѕРЅС‹: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
+    std::cout << "РЈРіР»С‹: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
 }
