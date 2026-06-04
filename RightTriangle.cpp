b8e8e3f0921e753bf498de8f3ea06483976c2c83
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-06-04 14:17:16 +0700

    Update RightTriangle.cpp

--- RightTriangle.cpp
+++ RightTriangle.cpp
@@ -9,12 +9,12 @@ RightTriangle::RightTriangle(int a, int b, int c, int A, int B)
 void RightTriangle::validate() const {
     Triangle::validate();
     if (C != 90) {
-        throw FigureException("Прямоугольный треугольник", "Угол C должен быть 90 градусов");
+        throw FigureException("РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє", "РЈРіРѕР» C РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ 90 РіСЂР°РґСѓСЃРѕРІ");
     }
 }
 
 void RightTriangle::print_info() const {
-    std::cout << "Прямоугольный треугольник:" << std::endl;
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+    std::cout << "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє:" << std::endl;
+    std::cout << "РЎС‚РѕСЂРѕРЅС‹: a=" << a << " b=" << b << " c=" << c << std::endl;
+    std::cout << "РЈРіР»С‹: A=" << A << " B=" << B << " C=" << C << std::endl;
 }
